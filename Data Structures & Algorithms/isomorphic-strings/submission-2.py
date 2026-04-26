a36a84eca67e923ed394a9fb0e388112f627b7f1
bsivavenu <30312991+bsivavenu@users.noreply.github.com>
2026-04-26 12:29:33 -0400

    Add: isomorphic-strings - submission-2

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # if len(set(s))!=len(set(t)):
+        #    return False
+
+        # x = {}
+        # for i,j in zip(s,t):
+        #     if i not in x:
+        #         x[i]=j
+            
+        #     elif x[i] != j:
+        #         return False
+
+        # return True
+        return list(map(s.find, s)) == list(map(t.find, t))
+        
+    
