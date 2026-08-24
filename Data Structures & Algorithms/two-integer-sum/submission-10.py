66d50bbdfe50529bb9503e424b3cdec9b5543beb
Siva Venu B <30312991+bsivavenu@users.noreply.github.com>
2026-08-23 20:35:33 -0400

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # x = {}
+        # for i,j in enumerate(nums):
+        #     y = target -j
+        #     if y in x:
+        #         return [x[y],i]
+        #     x[j] = i
+
+        
+
+        sorted_nums = sorted((num,idx) for (idx,num) in enumerate(nums))
+        print(sorted_nums)
+
+        left,right = 0, len(nums)-1
+
+        while left < right:
+
+            x = sorted_nums[left][0]+sorted_nums[right][0]
+            if x  == target :
+                return sorted([sorted_nums[left][1], sorted_nums[right][1]])
+            elif x < target:
+                left = left+1
+            else:
+                right = right-1
+        
